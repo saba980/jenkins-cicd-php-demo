46689332bdf6b521900cd81d9ec083827bf290cb
saba980 <sherwatto66@gmail.com>
2025-06-30 16:46:12 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! Declarative pipeline!!</p>'; ?> 
+ <?php echo '<p>Hello world from PHP! Declarative pipeline!! tera free style project </p>'; ?> 
  </body>
 </html>
